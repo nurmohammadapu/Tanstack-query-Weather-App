c661b89d1c8623dad362de216c9706c407ee9f41
Nur Mohammad Apu <mdapu2003@gmail.com>
2025-05-07 11:43:54 +0600

    Integrate Tanstack Query and update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,33 @@
+"use client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Navbar from "@/components/Navbar";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
-import Footer from './../components/Footer';
+import { ThemeProvider } from "@/components/theme-provider";
+import Footer from "./../components/Footer";
 
 export default function RootLayout({ children }: RootLayoutProps) {
+  const queryClient = new QueryClient();
   return (
     <>
       <html lang="en" suppressHydrationWarning>
         <head />
         <body className="bg-gradient-to-br from-background to-muted">
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Navbar/>
-            {children}
-            <Footer/>
-          </ThemeProvider>
+          <QueryClientProvider client={queryClient}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <Navbar />
+              {children}
+              <Footer />
+            </ThemeProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
         </body>
       </html>
     </>
-  )
+  );
 }
-
